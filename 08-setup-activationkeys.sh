8916680be6ccbf3645e36cfe8a126cee7afe3c53
Miki Shapiro <mikishapiro@users.noreply.github.com>
2025-04-17 10:28:11 +1000

    Update 08-setup-activationkeys.sh

--- 08-setup-activationkeys.sh
+++ 08-setup-activationkeys.sh
@@ -3,17 +3,17 @@
 
 # Create a key for the capsule:
 hammer activation-key create --organization MYMAINORG --auto-attach false --name ak_capsule --lifecycle-environment lce_prod --content-view cv_capsule
+# Make the capsule key enable the capsule dnf repositories when it is used:
+hammer activation-key content-override --organization MYMAINORG --name ak_capsule --content-label satellite-capsule-6.16-for-rhel-9-x86_64-rpms --override-name enabled --value 1
+hammer activation-key content-override --organization MYMAINORG --name ak_capsule --content-label satellite-maintenance-6.16-for-rhel-9-x86_64-rpms --override-name enabled --value 1
 
 # Create a key for the RHEL SOE (all orgs):
-
-# for org in MYMAINORG ORG2; do
-for org in MYMAINORG; do
-  for lce in stage nonprod prod; do
-    hammer activation-key create --organization $org --auto-attach false --name ak_rhel9_$lce --lifecycle-environment lce_$lce --content-view cv_rhel9
+for org in MYMAINORG ORG2; do
+  for os in rhel8 rhel9; do
+    # repeat for rhel8 and rhel9
+    for lce in stage nonprod prod; do
+      hammer activation-key create --organization $org --auto-attach false --name ak_${os}_${lce} --lifecycle-environment lce_$lce --content-view cv_$os
+    done
   done
-
-
-  # Make the capsule key enable the capsule dnf repositories when it is used:
-  hammer activation-key content-override --organization $org --name ak_capsule --content-label satellite-capsule-6.16-for-rhel-9-x86_64-rpms --override-name enabled --value 1
-  hammer activation-key content-override --organization $org --name ak_capsule --content-label satellite-maintenance-6.16-for-rhel-9-x86_64-rpms --override-name enabled --value 1
 done
+
